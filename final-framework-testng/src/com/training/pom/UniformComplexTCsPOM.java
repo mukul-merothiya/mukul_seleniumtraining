40b7b0c166118db0ab3407cb6b9268a565af95ce
mm3313 <mm3313@192.168.1.122>
2019-12-04 00:47:02 -0600

    third commit

---
+++ final-framework-testng/src/com/training/pom/UniformComplexTCsPOM.java
@@ -0,0 +1,396 @@
+package com.training.pom;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class UniformComplexTCsPOM {
+
+private WebDriver driver; 
+	
+	public UniformComplexTCsPOM(WebDriver driver) {
+		this.driver = driver; 
+		PageFactory.initElements(driver, this);
+	}
+	//Actions action=new Actions(driver);
+	
+	@FindBy(id="input-email")
+	private WebElement email;
+	@FindBy(id="input-password")
+	private WebElement password;
+	@FindBy(xpath="//input[@type='submit']")
+	private WebElement loginButton;
+	@FindBy(xpath="//*[@id=\"top-links1\"]/ul/li[3]/a")
+	private WebElement clickMyAccount;
+	@FindBy(xpath="//*[@id=\"top-links1\"]/ul/li[3]/ul/li[2]/a")
+	private WebElement clickloginLink;
+	
+	@FindBy(xpath="/html/body/div[1]/div[1]")
+	private WebElement warningMsg;
+	
+	@FindBy(xpath="//*[@id=\"banner0\"]/div/a/img")
+	private WebElement bannerlink;
+	
+	@FindBy(xpath="//*[@id=\"featured-grid\"]/div[1]/div/div/div[2]/div[2]/button[1]/span")
+	private WebElement addToCartBtn;
+	
+	@FindBy(xpath="//*[@id=\"featured-grid\"]/div[1]/div/div/div[2]/div[1]")
+	private WebElement clickFrameOnYellowTshirt;
+	
+	@FindBy(xpath="//*[@id=\"top-links1\"]/ul/li[3]/a/span[2]")
+	private WebElement clickAccountLogin;
+	
+	public void accountLoginButton()
+	{
+		this.clickAccountLogin.click();
+	}
+	
+	
+	@FindBy(xpath="//*[@id=\"top-links1\"]/ul/li[3]/ul/li[2]/a")
+	private WebElement loginlink;
+	
+	public void loginLinkClick()
+	{
+		this.loginlink.click();
+	}
+	
+	@FindBy(xpath="//*[@id=\"content\"]/div/div[2]/div/form/input")
+	private WebElement loginbutton;
+	
+	public void loginButton()
+	{
+		this.loginButton.click();
+	}
+	
+	@FindBy(xpath="//*[@id=\"logo\"]/h1/a")
+	private WebElement logo;
+	
+	public void clicklogo()
+	{
+		this.logo.click();
+	}
+	
+	
+	
+	public void sendEmail(String email) {
+		this.email.clear();
+		this.email.sendKeys(email);
+	}
+	
+	public void sendPassword(String password) {
+		this.password.clear(); 
+		this.password.sendKeys(password); 
+	}
+	
+	
+	public void clickLoginBtn() {
+		this.loginButton.click(); 
+	}
+
+	public void clickMyAccountLink() {
+		this.clickMyAccount.click();
+	}
+	public void clickLoginLink() {
+		this.clickloginLink.click();
+	}
+	
+	public String getWarningTxt()
+	{
+		return this.warningMsg.getText();
+	}
+	
+	public void clickBannerLink() {
+		this.bannerlink.click();
+	}
+
+	
+	public void clickAddToCartBtnForYellowTshirt() {
+		this.addToCartBtn.click();
+		//action.moveToElement(addToCartBtn).click().perform();
+	}
+	
+	public void clickFrameOnYellowTshirt() {
+		this.clickFrameOnYellowTshirt.click();
+		//action.moveToElement(addToCartBtn).click().perform();
+	}
+	
+	@FindBy(xpath="//*[@id=\"input-option382\"]")
+    private WebElement chestSize;
+	
+	public void clickChestSize()
+	{
+		this.chestSize.click();
+		Select selectProduct = new Select(chestSize);
+		selectProduct.selectByIndex(3);
+		this.chestSize.click();
+	}
+	
+	@FindBy(xpath="//*[@id=\"button-cart\"]")
+    private WebElement buttonCart;
+	
+	public void clickButtonCart() {
+		this.buttonCart.click();
+		//action.moveToElement(addToCartBtn).click().perform();
+	}
+	@FindBy(xpath="//*[@id=\"cart\"]")
+    private WebElement Cart;
+	
+	public void clickCart() {
+		this.Cart.click();
+		
+	}
+	@FindBy(xpath="//*[@id=\"cart\"]/ul/li[2]/div/p/a[1]/strong")
+    private WebElement viewCart;
+	
+	public void clickViewCart() {
+		this.viewCart.click();
+		
+	}
+	
+	@FindBy(xpath="//*[@id=\"content\"]/div[2]/div[2]/a")
+    private WebElement checkOutBtn;
+	
+	public void clickCheckOutBtn() {
+		this.checkOutBtn.click();
+		
+	}
+	
+	@FindBy(xpath="//*[@id=\"featured-grid\"]/div[2]/div/div/div[1]/a/img")
+	private WebElement clickFrameOnRedTshirt;
+	
+	public void clickAddToCartBtnForRedTshirt() {
+		this.clickFrameOnRedTshirt.click();
+		//action.moveToElement(addToCartBtn).click().perform();
+	}
+	
+	@FindBy(xpath="//*[@id=\"input-option376\"]")
+    private WebElement redchestSize;
+	
+	public void clickRedChestSize()
+	{
+		this.redchestSize.click();
+		Select selectProduct = new Select(redchestSize);
+		selectProduct.selectByIndex(3);
+		this.redchestSize.click();
+	}
+	
+	@FindBy(id="button-login")
+	private WebElement loginButtonCheckOut;
+	
+	public void clickloginButtonOnCheckOut()
+	{
+				this.loginButtonCheckOut.click();
+	}
+	
+	@FindBy(id="button-payment-address")
+	private WebElement continueButtonPaymentAddress;
+	
+	public void clickContinueButtonPaymentAddress()
+	{
+				this.continueButtonPaymentAddress.click();
+	}
+	
+	@FindBy(id="button-shipping-address")
+	private WebElement continueButtonShippingAddress;
+	
+	public void clickContinueButtonShippingAddress()
+	{
+				this.continueButtonShippingAddress.click();
+	}
+	@FindBy(xpath="//*[@id=\"collapse-shipping-method\"]/div/p[4]/textarea")
+	private WebElement deliveryMethodComment;
+	
+	public void deliveryMethodComment(String comments)
+	{
+				this.deliveryMethodComment.sendKeys(comments);
+	}
+	@FindBy(id="button-shipping-method")
+	private WebElement continueButtonShippingMethod;
+	
+	public void clickContinueButtonShippingMethod()
+	{
+				this.continueButtonShippingMethod.click();
+	}
+	
+	@FindBy(xpath="//*[@id=\"collapse-payment-method\"]/div/div[2]/div/input[1]")
+	private WebElement readAndAgree;
+	
+	public void clickReadandAgreeCheckbox()
+	{
+				this.readAndAgree.click();
+	}
+	@FindBy(id="button-payment-method")
+	private WebElement continueButtonPaymentMethod;
+	
+	public void clickContinueButtonPaymentMethod()
+	{
+				this.continueButtonPaymentMethod.click();
+	}
+	@FindBy(id="button-confirm")
+	private WebElement buttonConfirm;
+	
+	public void clickButtonConfirm()
+	{
+				this.buttonConfirm.click();
+	}
+	@FindBy(xpath="//*[@id=\"collapse-checkout-option\"]/div/div/div[1]/div[2]/label/input")
+	private WebElement guestRadiobutton;
+	
+	public void clickGuestRadioButton()
+	{
+				this.guestRadiobutton.click();
+	}
+
+	
+	@FindBy(id="button-account")
+	private WebElement continueButtonAccount;
+	
+	public void clickContinueButtonAccount()
+	{
+				this.continueButtonAccount.click();
+	}
+	
+	@FindBy(id="input-payment-firstname")
+	private WebElement paymentFirstName;
+	
+	public void sendPaymentFirstName(String paymentFirstName)
+	{
+				this.paymentFirstName.sendKeys(paymentFirstName);
+	}
+	
+	@FindBy(id="input-payment-lastname")
+	private WebElement paymentLastName;
+	
+	public void sendPaymentlastName(String PaymentLastName)
+	{
+				this.paymentLastName.sendKeys(PaymentLastName);
+	}
+	
+	@FindBy(id="input-payment-address-1")
+	private WebElement paymentAddress1;
+	
+	public void sendPaymentAddress1(String Address1)
+	{
+				this.paymentAddress1.sendKeys(Address1);
+	}
+	@FindBy(id="input-payment-email")
+	private WebElement paymentEmail;
+	
+	public void sendPaymentEmail(String Email)
+	{
+				this.paymentEmail.sendKeys(Email);
+	}
+	@FindBy(xpath="//*[@id=\"input-payment-telephone\"]")
+	private WebElement paymentTelephone;
+	
+	public void sendPaymentTelephone(String paymentTelephone)
+	{
+				this.paymentTelephone.sendKeys(paymentTelephone);
+	}
+	@FindBy(id="input-payment-city")
+	private WebElement paymentCity;
+	
+	public void sendPaymentCity(String paymentcity)
+	{
+				this.paymentCity.sendKeys(paymentcity);
+	}
+	@FindBy(id="input-payment-postcode")
+	private WebElement paymentPostcode;
+	
+	public void sendPaymentPasscode(String paymentPostcode)
+	{
+				this.paymentPostcode.sendKeys(paymentPostcode);
+	}
+	@FindBy(id="input-payment-country")
+    private WebElement paymentCountry;
+	
+	public void selectPaymentCountry()
+	{
+		this.paymentCountry.click();
+		Select selectProduct = new Select(paymentCountry);
+		selectProduct.selectByVisibleText("India");
+		this.paymentCountry.click();
+	}
+	@FindBy(id="input-payment-zone")
+    private WebElement paymentState;
+	
+	public void selectPaymentState()
+	{
+		this.paymentState.click();
+		Select selectProduct = new Select(paymentState);
+		selectProduct.selectByVisibleText("Karnataka");
+		this.paymentState.click();
+	}
+	
+	@FindBy(id="button-guest")
+	private WebElement continueButtonGuest;
+	
+	public void clickContinueButtonGuest()
+	{
+				this.continueButtonGuest.click();
+	}
+	
+	
+	@FindBy(xpath="//a[@data-original-title='Add New']")
+	private WebElement addNewButton;
+	
+	public void clickAddNewBtn()
+	{
+		this.addNewButton.click();
+	}
+	
+	@FindBy(id="input-name1")
+	private WebElement productName;
+	
+	public void sendProductName(String productName)
+	{
+		this.productName.sendKeys(productName);
+	}
+	
+	@FindBy(id="input-name1")
+	private WebElement metaTitle;
+	
+	public void sendMetaTitle(String metaTitle)
+	{
+		this.metaTitle.sendKeys(metaTitle);
+	}
+	
+	@FindBy(xpath="//*[@id=\"form-order\"]/div/table/tbody/tr[1]/td[8]/a[1]")
+	private WebElement orderViewbtn;
+	
+	public void clickOrderViewEye()
+	{
+		this.orderViewbtn.click();
+	}
+	@FindBy(id="input-order-status")
+    private WebElement orderStatus;
+	
+	public void clickOrderStatus()
+	{
+		this.orderStatus.click();
+		Select selectProduct = new Select(orderStatus);
+		selectProduct.selectByVisibleText("Complete");
+	}
+	@FindBy(id="button-history")
+    private WebElement historybutton;
+	
+	public void clickHistoryButton()
+	{
+		this.historybutton.click();
+		
+	}
+	
+	@FindBy(xpath="//*[@id=\"top-links1\"]/ul/li[3]/ul/li[2]/a")
+	private WebElement orderHistory;
+	
+	public void clickOrderHistory()
+	{
+		this.orderHistory.click();
+	}
+
+	
+	
+}
--- final-framework-testng/src/com/training/pom/UniformPOM.java
+++ final-framework-testng/src/com/training/pom/UniformPOM.java
@@ -3,151 +3,283 @@ package com.training.pom;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class UniformPOM {
-	private WebDriver driver; 
-	
+	private WebDriver driver;
+
 	public UniformPOM(WebDriver driver) {
-		this.driver = driver; 
+		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
-	@FindBy(id="input-username")
-	private WebElement userName; 
-	
-	@FindBy(id="input-password")
+
+	@FindBy(id = "input-username")
+	private WebElement userName;
+
+	@FindBy(id = "input-password")
 	private WebElement password;
-	
-	@FindBy(xpath="//*[@id=\'content\']/div/div/div/div/div[2]/form/div[3]/button")
+
+	@FindBy(xpath = "//*[@id=\'content\']/div/div/div/div/div[2]/form/div[3]/button")
 	private WebElement loginBtn;
-	
-	@FindBy(xpath="//*[@id=\"input-name\"]")
+
+	@FindBy(xpath = "//*[@id=\"input-name\"]")
 	private WebElement prodName;
-	
+
 	public void sendUserName(String userName) {
 		this.userName.clear();
 		this.userName.sendKeys(userName);
 	}
-	
+
 	public void sendPassword(String password) {
-		this.password.clear(); 
-		this.password.sendKeys(password); 
-	}
-	
-	public void clickLoginBtn() {
-		this.loginBtn.click(); 
+		this.password.clear();
+		this.password.sendKeys(password);
 	}
 
-	@FindBy(xpath="//*[@id=\'button-menu\']")
-	private WebElement catlogiconBtn; 
-	
-	@FindBy(xpath="//*[@id=\"catalog\"]/a/span")
-	private WebElement prodLink; 
-	
-	@FindBy(xpath="//*[@id=\"catalog\"]/ul/li[2]/a")
+	public void clickLoginBtn() {
+		this.loginBtn.click();
+	}
+
+	@FindBy(xpath = "//*[@id=\'button-menu\']")
+	private WebElement catlogiconBtn;
+
+	@FindBy(xpath = "//*[@id=\"catalog\"]/a/span")
+	private WebElement prodLink;
+
+	@FindBy(xpath = "//*[@id=\"catalog\"]/ul/li[2]/a")
 	private WebElement prodLink1;
-	
-	@FindBy(xpath="//*[@id=\"form-product\"]/div/table/tbody/tr[1]/td[1]/input")
+
+	@FindBy(xpath = "//*[@id=\"form-product\"]/div/table/tbody/tr[1]/td[1]/input")
 	private WebElement tableChkbox;
-	
-	@FindBy(id="button-filter")
+
+	@FindBy(id = "button-filter")
 	private WebElement buttonFilter;
-	
-	@FindBy(xpath="//*[@id=\"content\"]/div[1]/div/div/button[2]")
+
+	@FindBy(xpath = "//*[@id=\"content\"]/div[1]/div/div/button[2]")
 	private WebElement deleteProduct;
-	
-	@FindBy(xpath="//*[@id=\"content\"]/div[2]/div[1]")
+
+	@FindBy(xpath = "//*[@id=\"content\"]/div[2]/div[1]")
 	private WebElement successText;
-	
-	@FindBy(xpath="//*[@id=\"sale\"]/a/span")
+
+	@FindBy(xpath = "//*[@id=\"sale\"]/a/span")
 	private WebElement salesIcon;
-	
-	
+
 	public void clickCatlogIconBtn() {
 		this.catlogiconBtn.click();
 	}
+
 	public void clickProdLink() {
-		this.prodLink.click(); 
+		this.prodLink.click();
 	}
+
 	public void clickProdSubLink() {
-		this.prodLink1.click(); 
+		this.prodLink1.click();
 	}
-	
+
 	public void prodName(String prodName) {
 		this.prodName.sendKeys(prodName);
-		
+
 	}
-	public void clickProductFilterButton()
-	{
+
+	public void clickProductFilterButton() {
 		this.buttonFilter.click();
 	}
-	public void clickTableCheckBox()
-	{
+
+	public void clickTableCheckBox() {
 		this.tableChkbox.click();
 	}
-	public void clickDeleteProduct()
-	{
+
+	public void clickDeleteProduct() {
 		this.deleteProduct.click();
 	}
-	
-	public String successAlertText()
-	{
+
+	public String successAlertText() {
 		return this.successText.getText();
-		
+
 	}
-	public void salesIcon()
-	{
+
+	public void salesIcon() {
 		this.salesIcon.click();
 	}
-	@FindBy(xpath="//*[@id=\"sale\"]/ul/li[1]/a")
-    private WebElement orderLink;
-	
-	public void clickOrderLink()
-	{
+
+	@FindBy(xpath = "//*[@id=\"sale\"]/ul/li[1]/a")
+	private WebElement orderLink;
+
+	public void clickOrderLink() {
 		this.orderLink.click();
 	}
-	
-	@FindBy(id="input-order-status")
-    private WebElement orderStatus;
-	
-	public void clickOrderStatus()
-	{
+
+	@FindBy(id = "input-order-status")
+	private WebElement orderStatus;
+
+	public void clickOrderStatus() {
 		this.orderStatus.click();
 		Select selectProduct = new Select(orderStatus);
 		selectProduct.selectByVisibleText("Pending");
 	}
-	
-	@FindBy(id="button-filter")
+
+	@FindBy(id = "button-filter")
 	private WebElement buttonOrderFilter;
-	
-	public void clickOrderFilterButton()
-	{
+
+	public void clickOrderFilterButton() {
 		this.buttonOrderFilter.click();
 	}
-	
-	
-	@FindBy(id="input-date-added")
+
+	@FindBy(id = "input-date-added")
 	private WebElement dateAdded;
-	
+
 	public void sendDateAdded(String dateAdded) {
-		this.dateAdded.clear(); 
-		this.dateAdded.sendKeys(dateAdded); 
+		this.dateAdded.clear();
+		this.dateAdded.sendKeys(dateAdded);
 	}
-	
-	@FindBy(id="input-date-modified")
+
+	@FindBy(id = "input-date-modified")
 	private WebElement dateModified;
-	
+
 	public void sendDateModified(String dateModified) {
-		this.dateModified.clear(); 
-		this.dateModified.sendKeys(dateModified); 
+		this.dateModified.clear();
+		this.dateModified.sendKeys(dateModified);
+	}
+
+	@FindBy(xpath = "//*[@id=\"content\"]/div[1]/div/div/a")
+	private WebElement addNewProduct;
+
+	public void clickAddNewProduct() {
+		this.addNewProduct.click();
+	}
+
+	@FindBy(id = "input-name1")
+	private WebElement productName;
+
+	public void sendProductName(String productName) {
+		this.productName.clear();
+		this.productName.sendKeys(productName);
+	}
+
+	@FindBy(id = "input-meta-title1")
+	private WebElement metatagtitle;
+
+	public void sendMetaTagTitle(String metatagtitle) {
+		this.metatagtitle.clear();
+		this.metatagtitle.sendKeys(metatagtitle);
+	}
+
+	@FindBy(xpath = "//*[@id=\"form-product\"]/ul/li[2]/a")
+	private WebElement dataTab;
+
+	public void clickDataTab() {
+		this.dataTab.click();
+	}
+
+	@FindBy(id = "input-model")
+	private WebElement model;
+
+	public void sendModel(String model) {
+
+		this.model.sendKeys(model);
+	}
+
+	@FindBy(xpath = "//*[@id=\"input-price\"]")
+	private WebElement price1;
+
+	public void sendPrice(String price1) {
+		this.model.sendKeys(price1);
+	}
+
+	@FindBy(xpath = "//*[@id=\"input-quantity\"]")
+	private WebElement quantity;
+
+	public void sendQuantity(String quantity) {
+
+		this.model.sendKeys(quantity);
+	}
+
+	@FindBy(xpath = "//*[@id=\"form-product\"]/ul/li[3]/a")
+	private WebElement linkTab;
+
+	public void clickLinkTab() {
+		this.linkTab.click();
+	}
+
+	/*
+	 * @FindBy(id = "input-category") private WebElement categories; public void
+	 * clickCategories() { this.categories.clear(); }
+	 * 
+	 * public void selectCategories() {
+	 * 
+	 * Select selectProduct = new Select(categories);
+	 * selectProduct.selectByVisibleText("Blazer"); }
+	 */
+
+	@FindBy(xpath = "//*[@id=\"form-product\"]/ul/li[7]/a")
+	private WebElement discountTab;
+
+	public void clickdiscountTab() {
+		this.discountTab.click();
+	}
+
+	@FindBy(xpath = "//*[@id=\"discount\"]/tfoot/tr/td[2]/button")
+	private WebElement addDiscount;
+
+	public void clickAddDiscount() {
+		this.addDiscount.click();
+	}
+
+	@FindBy(xpath = "//*[@id=\"discount-row0\"]/td[2]/input")
+	private WebElement discountQty;
+
+	public void sendDiscountQty(String discountQty) {
+		this.discountQty.sendKeys(discountQty);
+	}
+
+	@FindBy(xpath = "//*[@id=\"discount-row0\"]/td[4]/input")
+	private WebElement discountPrice;
+
+	public void sendDiscountPrice(String discountPrice) {
+		this.discountPrice.sendKeys(discountPrice);
+	}
+
+	@FindBy(xpath = "//*[@id=\"discount-row0\"]/td[5]/div/input")
+	private WebElement discountStartDate;
+
+	public void sendDiscountStartDate(String discountStartDate) {
+		this.discountStartDate.sendKeys(discountStartDate);
+
+	}
+	
+	@FindBy(xpath = "//*[@id=\"discount-row0\"]/td[6]/div/input")
+	private WebElement discountEndDate;
+
+	public void sendDiscountEndDate(String discountEndDate) {
+		this.discountEndDate.sendKeys(discountEndDate);
+
 	}
 	
 	
+	@FindBy(xpath = "//*[@id=\"form-product\"]/ul/li[10]/a")
+	private WebElement addRewardPoint;
+
+	public void clickRewardPointTab() {
+		this.addRewardPoint.click();
+	}
+	@FindBy(xpath = "//*[@id=\"input-points\"]")
+	private WebElement rewardpointValue;
+
+	public void sendrewardPoint(String rewardpointValue) {
+		this.rewardpointValue.sendKeys(rewardpointValue);
+
+	}
 	
+	
+	@FindBy(xpath = "//*[@id=\"content\"]/div[1]/div/div/button")
+	private WebElement saveBtn;
+
+	public void clickSaveBtn() {
+		this.saveBtn.click();
+	}
+	
+	
+
 }
-
-
-
